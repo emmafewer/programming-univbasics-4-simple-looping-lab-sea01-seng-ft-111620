7adf54f949a9019d05cb25cc34b032187493da00
Emma Fewer <emmafewer20@gmail.com>
2020-10-16 22:02:52 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -2,15 +2,15 @@ def loop_message_five_times(string)
   count = 0 
   while count < 5 do 
    puts string
-   count +=1 
+   count += 1 
   end
 end
 
 def loop_message_n_times(string, integer)
   count = 0 
-  while count<=integer do
+  while count<integer do
    puts string 
-   count +=1 
+   count += 1 
   end
 end
 
@@ -18,7 +18,7 @@ def output_array(array)
   count = 0 
   while count<array.length do
    puts array[count]
-   count +=l 
+   count += l 
   end
 end
 
